ea77c0ab3866cbb95a34e48ba78c48f86a3a250f
obong samson <obongsamson00@gmail.com>
2023-12-29 16:49:12 +0100

    Your commit message

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
+function App() {
+
+  return (
+  <Router>
+    <div className="App">
+      <Navbar /> 
+      <div className="content">
+        <switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+
+          <Route path='/Create'>
+           <Create />
+          </Route>
+
+          <Route path='/blogs/:id'>
+           <BlogDetails />
+          </Route>
+          <Route  path='*'>
+            <NotFound />
+          </Route>
+        </switch>
+      </div>
+    </div>
+  </Router>
+  );
+}
+
+export default App;
